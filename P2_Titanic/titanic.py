ecd279561e04d39382e2cc21af7ba8d28c7e627d
Gaziz Kugushev <kugushev@gmail.com>
2017-03-19 16:47:49 -0400

    added README.md for data directory

---
+++ P2_Titanic/titanic.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+import seaborn
+import numpy as np
+import pandas as pd
+
+
+
+passengers = pd.read_csv('data/titanic-data.csv')
+print(passengers.axes)
+
+print(passengers['Age'])
+
